8c8e042e839a03f8c068f5b063f6b318834d400e
Jérémie Bernard <jeremie.bernard.1@ulaval.ca>
2024-05-09 23:20:01 -0400

    INV-43: local storage and use effect

--- inventaire/src/main.tsx
+++ inventaire/src/main.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App.tsx';
+import App from './App.tsx';
 // import TestApp from './TestApp.tsx';
-import Inventory from './Inventory.tsx';
+// import Inventory from './Inventory.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
     {/* <TestApp /> */}
-    <Inventory />
+    {/* <Inventory /> */}
   </React.StrictMode>
 );
