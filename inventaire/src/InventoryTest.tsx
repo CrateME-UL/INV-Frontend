46479f7706bb631b2223d742cb7d11743c77d4cf
Jérémie Bernard <jeremie.bernard.1@ulaval.ca>
2024-05-26 13:31:52 -0400

    INV-52: :lipstick: add material-icon

--- inventaire/src/InventoryTest.tsx
+++ inventaire/src/InventoryTest.tsx
@@ -1,9 +1,23 @@
 import * as React from 'react';
 import axios from 'axios';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import styles from './styles/styles.module.css';
+import IconButton from '@mui/material/IconButton';
+import RefreshIcon from '@mui/icons-material/Refresh';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const columnsTest: GridColDef[] = [
+  { field: 'nbOfItems', headerName: 'Qte', width: 100 },
+  { field: 'itemName', headerName: 'Type', width: 300 },
+];
 
 //TODO: add env variable for link to server
 //TODO: add components library and style UI
-//TODO: refactor InventoryTest in seperate files
+//TODO: refactor InventoryTest in separate files
 //TODO: add tests
 //TODO: add places instead of numbers -> implement different request server side
 
@@ -32,13 +46,16 @@ const ItemDtoToModel = (item: ItemDto): ItemModel => ({
 
 const App = () => {
   const [items, setItems] = React.useState<ItemModel[]>([]);
+  const [error, setError] = React.useState<string | null>(null);
+
   const fetchItems = async () => {
     try {
       const result = await axios.get(API_ENDPOINT);
       const itemModels: ItemModel[] = result.data.map(ItemDtoToModel);
       setItems(itemModels);
+      setError(null);
     } catch {
-      console.log('Something went wrong ...');
+      setError('Something went wrong...');
     }
   };
 
@@ -48,28 +65,57 @@ const App = () => {
 
   return (
     <>
-      <h1>Hello</h1>
-      <button type="button" onClick={fetchItems}>
-        fetch items
-      </button>
-      <List items={items} />
+      {/* <ButtonAppBar /> */}
+      <Box display="flex" alignItems="center" justifyContent="left">
+        <IconButton aria-label="refresh" onClick={fetchItems}>
+          <RefreshIcon />
+        </IconButton>
+        <Typography component="span">Inventaire</Typography>
+      </Box>
+      {error ? (
+        <p>{error}</p>
+      ) : (
+        <div className={styles.list}>
+          <DataGrid
+            rows={items}
+            columns={columnsTest}
+            getRowId={(row) => row.itemId}
+            initialState={{
+              pagination: {
+                paginationModel: { page: 0, pageSize: 5 },
+              },
+            }}
+            pageSizeOptions={[5, 10]}
+          />
+        </div>
+      )}
     </>
   );
 };
 
-const List = ({ items }: { items: ItemModel[] }) => (
-  <ul>
-    {items.map((item: ItemModel) => (
-      <Item key={item.itemId} item={item} />
-    ))}
-  </ul>
-);
-
-const Item = ({ item }: { item: ItemModel }) => (
-  <li key={item.itemId}>
-    {item.nbOfItems} {item.itemName}: emplacement {'-> '}
-    {item.placeId}
-  </li>
-);
+export function ButtonAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1 }}
+          ></Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
 
 export default App;
